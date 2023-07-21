aee22acf632df43add037ec2e36a1177a1602ca5
Ana de Souza <anaemerimdsz@gmail.com>
2023-07-21 10:50:01 -0300

    Adjust retry timeout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import BookList from "./components/BookList";
 import "./App.css";
 import axios from "axios";
 
 const apiKey = process.env.REACT_APP_NYT_BOOKS_KEY;
 
-// Axios instance with retry configuration
 const axiosInstance = axios.create({
   baseURL: "https://api.nytimes.com",
-  timeout: 5000, // Set a timeout (optional)
+  timeout: 5000,
 });
 
 axiosInstance.interceptors.response.use(
@@ -16,8 +15,8 @@ axiosInstance.interceptors.response.use(
   (error) => {
     const status = error.response ? error.response.status : null;
     if (status === 429) {
-      console.log("Rate limit exceeded. Retrying after 2 seconds...");
-      return new Promise((resolve) => setTimeout(resolve, 3500)).then(() =>
+      console.log("Rate limit exceeded. Retrying after 3 seconds...");
+      return new Promise((resolve) => setTimeout(resolve, 3000)).then(() =>
         axiosInstance(error.config)
       );
     }
@@ -25,72 +24,63 @@ axiosInstance.interceptors.response.use(
   }
 );
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      lists: [],
-      bestSellers: [],
-    };
-  }
+function App() {
+  const [lists, setLists] = useState([]);
+  const [bestSellers, setBestSellers] = useState([]);
 
-  async componentDidMount() {
-    await this.fetchLists();
-    await this.fetchBestSellers();
-  }
+  useEffect(() => {
+    fetchLists();
+    fetchBestSellers();
+  }, []);
 
-  fetchLists = async () => {
+  const fetchLists = async () => {
     try {
       const response = await axiosInstance.get(
         `/svc/books/v3/lists/full-overview?api-key=${apiKey}`
       );
       const listsData = response.data.results.lists;
-      this.setState({ lists: listsData });
+      setLists(listsData);
     } catch (error) {
       console.error("Error fetching book lists:", error);
     }
   };
 
-  fetchBestSellers = async () => {
+  const fetchBestSellers = async () => {
     try {
       const response = await axiosInstance.get(
         `/svc/books/v3/lists/best-sellers/history.json?api-key=${apiKey}`
       );
       const bestSellersData = response.data.results;
-      this.setState({ bestSellers: bestSellersData });
+      setBestSellers(bestSellersData);
     } catch (error) {
       console.error("Error fetching best sellers:", error);
     }
   };
 
-  render() {
-    const { lists, bestSellers } = this.state;
-
-    return (
-      <div className="app-container">
-        <header className="header">
-          <h2>NYT Best Sellers</h2>
-        </header>
-        <main className="main-body">
-          <div className="book-list-wrapper">
-            <h2>Best Sellers</h2>
-            <BookList books={bestSellers} />
+  return (
+    <div className="app-container">
+      <header className="header">
+        <h2>NYT Best Sellers</h2>
+      </header>
+      <main className="main-body">
+        <div className="book-list-wrapper">
+          <h2>Best Sellers</h2>
+          <BookList books={bestSellers} />
+        </div>
+        {lists.map((list) => (
+          <div className="book-list-wrapper" key={list.list_id}>
+            <h2>{list.list_name}</h2>
+            <BookList books={list.books} />
           </div>
-          {lists.map((list) => (
-            <div className="book-list-wrapper" key={list.list_id}>
-              <h2>{list.list_name}</h2>
-              <BookList books={list.books} />
-            </div>
-          ))}
-        </main>
-        <footer className="app-footer">
-          <a href="https://github.com/anaemdsz/nyt-book-challenge">Github</a>
-          <div>-</div>
-          <a href="https://developer.nytimes.com/docs/books-product/1/overview">NYT API</a>
-        </footer>
-      </div>
-    );
-  }
+        ))}
+      </main>
+      <footer className="app-footer">
+        <a href="https://github.com/anaemdsz/nyt-book-challenge">Github</a>
+        <div>-</div>
+        <a href="https://developer.nytimes.com/docs/books-product/1/overview">NYT API</a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
