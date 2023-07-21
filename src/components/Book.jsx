0ad40093802fb57f45d2736b48875552e503dff0
Ana de Souza <anaemerimdsz@gmail.com>
2023-07-21 10:49:48 -0300

    Convert classes into functions.

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,42 +1,38 @@
-import React, { Component } from "react";
+import React from "react";
 import "./Book.css";
 
-class Book extends Component {
-  render() {
-    const { book } = this.props;
-
-    return (
-      <div id={book.rank} className="entry" data-text={book.description}>
-        <div className="book-image">
-          {book.book_image ? (
-            <img
-              src={book.book_image}
-              className="cover-image"
-              id={"cover-" + book.rank}
-              alt={book.title}
-            />
-          ) : (
-            <img
-              src={process.env.PUBLIC_URL + "/cover_image.jpg"}              className="cover-image"
-              id={"cover-" + book.rank}
-              alt={book.title}
-            />
-          )}
-        </div>
-        <div className="book-details">
-          <h2 className="book-title">
-            <a href={book.amazon_product_url} target="_blank" rel="noreferrer">
-              {book.rank ? `#${book.rank}: ` : ""} {book.title}
-            </a>
-          </h2>
-          <h4 className="book-author">By {book.author}</h4>
-          <h4 className="book-publisher">{book.publisher}</h4>
-          <div className="book-stats">
-          </div>
-        </div>
+function Book({ book }) {
+  return (
+    <div id={book.rank} className="entry" data-text={book.description}>
+      <div className="book-image">
+        {book.book_image ? (
+          <img
+            src={book.book_image}
+            className="cover-image"
+            id={"cover-" + book.rank}
+            alt={book.title}
+          />
+        ) : (
+          <img
+            src={process.env.PUBLIC_URL + "/cover_image.jpg"}
+            className="cover-image"
+            id={"cover-" + book.rank}
+            alt={book.title}
+          />
+        )}
       </div>
-    );
-  }
+      <div className="book-details">
+        <h2 className="book-title">
+          <a href={book.amazon_product_url} target="_blank" rel="noreferrer">
+            {book.rank ? `#${book.rank}: ` : ""} {book.title}
+          </a>
+        </h2>
+        <h4 className="book-author">By {book.author}</h4>
+        <h4 className="book-publisher">{book.publisher}</h4>
+        <div className="book-stats"></div>
+      </div>
+    </div>
+  );
 }
 
 export default Book;
--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,41 +1,34 @@
-import React, { Component } from "react";
+import React, { useRef } from "react";
 import Book from "./Book";
 import "./BookList.css";
 
-class BookList extends Component {
-  constructor(props) {
-    super(props);
-    this.listRef = React.createRef(); // Reference to the book list container
-    this.scrollStep = 300; // Set the scroll step size here (e.g., 100 pixels)
-  }
+function BookList({ books }) {
+  const listRef = useRef(null); // Reference to the book list container
+  const scrollStep = 300; // Set the scroll step size here (e.g., 100 pixels)
 
-  scrollLeft = () => {
-    this.listRef.current.scrollLeft -= this.scrollStep;
+  const scrollLeft = () => {
+    listRef.current.scrollLeft -= scrollStep;
   };
 
-  scrollRight = () => {
-    this.listRef.current.scrollLeft += this.scrollStep;
+  const scrollRight = () => {
+    listRef.current.scrollLeft += scrollStep;
   };
 
-  render() {
-    const { books } = this.props;
-
-    return (
-      <div className="book-list-container">
-        <button className="book-list-scroll" onClick={this.scrollLeft}>
-          &lt;
-        </button>
-        <div className="book-list" ref={this.listRef}>
-          {books.map((book) => (
-            <Book key={book.rank} book={book} />
-          ))}
-        </div>
-        <button className="book-list-scroll" onClick={this.scrollRight}>
-          &gt;
-        </button>
+  return (
+    <div className="book-list-container">
+      <button className="book-list-scroll" onClick={scrollLeft}>
+        &lt;
+      </button>
+      <div className="book-list" ref={listRef}>
+        {books.map((book) => (
+          <Book key={book.rank} book={book} />
+        ))}
       </div>
-    );
-  }
+      <button className="book-list-scroll" onClick={scrollRight}>
+        &gt;
+      </button>
+    </div>
+  );
 }
 
 export default BookList;
